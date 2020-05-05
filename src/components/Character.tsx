ef677bc4b351e229ad15eb099c99e86bbedc5395
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-05-05 18:26:30 +0900

    캐릭터 로그인 수정

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
