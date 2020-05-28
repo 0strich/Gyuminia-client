31322c085c66d13d384502fe3f84c7bb23c77a8c
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-05-28 14:01:45 +0900

    delete : 기존 로그인 걷어내기

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
-import Login from "./Login";
 import Signup from "./Signup";
 
 const Auth = () => {
@@ -10,7 +9,7 @@ const Auth = () => {
   // 인증 타입별로 컴포넌트 분기
   const authQurter = (): any => {
     if (type === "login" || type === "signup") {
-      return type === "login" ? <Login /> : <Signup />;
+      return type === "login" ? null : <Signup />;
     } else {
       return history.push("/");
     }
