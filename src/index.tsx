dbd6c264d24395d0d6429193456f2fad09177bec
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-06-02 02:18:40 +0900

    feat : redux-persist 재적용

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,28 @@ import { Provider } from "react-redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./modules";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import Loading from "./components/etc/Loading";
 
 export const isEmpty = require("lodash.isempty");
 export const history = require("history").createBrowserHistory();
 
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const enhancedReducer = persistReducer(persistConfig, reducer);
+const store = createStore(enhancedReducer, applyMiddleware(logger, thunk));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<Loading />} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
